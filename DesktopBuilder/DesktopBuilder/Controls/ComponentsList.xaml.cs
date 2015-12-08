76e5bfe25785459c09adf25f5a6a8310a371704d
tuanlt2703 <tuanlt2703@gmail.com>
2015-12-08 09:48:17 +0700

    Update UI
    Mainpage selection, ComponentsList, Product

---
+++ DesktopBuilder/DesktopBuilder/Controls/ComponentsList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopBuilder.Controls
+{
+    /// <summary>
+    /// Interaction logic for ComponentsList.xaml
+    /// </summary>
+    public partial class ComponentsList : UserControl
+    {
+        public ComponentsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DesktopBuilder/DesktopBuilder/MainWindow.xaml.cs
+++ DesktopBuilder/DesktopBuilder/MainWindow.xaml.cs
@@ -20,9 +20,52 @@ namespace DesktopBuilder
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+            recSelected.Add(recComponentSelected);
+            recSelected.Add(recAdvisorSelected);
+            recSelected.Add(recCustomSelected);
+            gridSelected.Add(gridComponents);
+            gridSelected.Add(gridAdvisor);
+            gridSelected.Add(gridCustom);
         }
+        #endregion
+
+        #region Properties
+        private List<Rectangle> recSelected = new List<Rectangle>();
+        private List<Grid> gridSelected = new List<Grid>();
+        #endregion
+
+        #region Events
+        private void btnComponents_Click(object sender, RoutedEventArgs e)
+        {
+            Alternate(0);
+        }
+        private void btnAdvisor_Click(object sender, RoutedEventArgs e)
+        {
+            Alternate(1);
+        }
+        private void btnCustom_Click(object sender, RoutedEventArgs e)
+        {
+            Alternate(2);
+        }
+        #endregion
+
+        #region Methods
+        private void Alternate(int index)
+        {
+            recSelected[index].Visibility = Visibility.Visible;
+            gridSelected[index].Visibility = Visibility.Visible;
+            int tmp = index + 1;
+            while ((tmp % 3) != index)
+            {
+                recSelected[tmp % 3].Visibility = Visibility.Hidden;
+                gridSelected[tmp % 3].Visibility = Visibility.Hidden;
+                tmp++;
+            }
+        }
+        #endregion
     }
 }
